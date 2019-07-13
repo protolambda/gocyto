231777f18ac0f9f36f2bdd62162e1c75b5921f7d
protolambda <proto@protolambda.com>
2019-07-13 04:03:00 +0200

    Analysis code to load SSA program and construct callgraph

---
+++ analysis/analysis.go
@@ -0,0 +1,115 @@
+package analysis
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"golang.org/x/tools/go/callgraph"
+	"golang.org/x/tools/go/callgraph/cha"
+	"golang.org/x/tools/go/callgraph/rta"
+	"golang.org/x/tools/go/pointer"
+
+	"golang.org/x/tools/go/callgraph/static"
+	"golang.org/x/tools/go/packages"
+	"golang.org/x/tools/go/ssa"
+	"golang.org/x/tools/go/ssa/ssautil"
+	"log"
+)
+
+type ProgramAnalysis struct {
+	Prog  *ssa.Program
+	Pkgs  []*ssa.Package
+	Mains []*ssa.Package
+}
+
+const pkgLoadMode = packages.NeedName |
+	packages.NeedFiles |
+	packages.NeedCompiledGoFiles |
+	packages.NeedImports |
+	packages.NeedDeps |
+	packages.NeedExportsFile |
+	packages.NeedTypes |
+	packages.NeedSyntax |
+	packages.NeedTypesInfo |
+	packages.NeedTypesSizes
+
+type AnalysisMode uint64
+
+const (
+	PointerAnalysis AnalysisMode = iota
+	StaticAnalysis
+	ClassHierarchyAnalysis
+	RapidTypeAnalysis
+)
+
+func RunAnalysis(withTests bool, buildFlags []string, pkgPatterns []string) (*ProgramAnalysis, error) {
+	conf := &packages.Config{
+		Mode:  pkgLoadMode,
+		Tests: withTests,
+		BuildFlags: buildFlags,
+	}
+	loaded, err := packages.Load(conf, pkgPatterns...)
+	if err != nil {
+		log.Fatalln("failed packages load:", err)
+	}
+
+	// TODO: maybe just depend on pattern match covering all desired packages?
+	// add imported packages
+	for i, end := 0, len(loaded); i < end; i++ {
+		p := loaded[i]
+		for _, imp := range p.Imports {
+			loaded = append(loaded, imp)
+		}
+	}
+	prog, initialPkgs := ssautil.Packages(loaded, 0)
+
+	var errorMsg bytes.Buffer
+	for i, p := range initialPkgs {
+		if p == nil {
+			errorMsg.WriteString("failed to get SSA for pkg: ")
+			errorMsg.WriteString(loaded[i].PkgPath)
+			errorMsg.WriteString("\n")
+		}
+	}
+	if errorMsg.Len() != 0 {
+		return nil, errors.New(errorMsg.String())
+	}
+
+	prog.Build()
+
+	pkgs := prog.AllPackages()
+	mains := ssautil.MainPackages(pkgs)
+
+	return &ProgramAnalysis{
+		Prog:  prog,
+		Pkgs:  pkgs,
+		Mains: mains,
+	}, nil
+}
+
+func (mode AnalysisMode) LoadCallgraph(data *ProgramAnalysis) *callgraph.Graph {
+	switch mode {
+	case PointerAnalysis:
+		ptrcfg := &pointer.Config{
+			Mains:          data.Mains,
+			BuildCallGraph: true,
+		}
+		result, err := pointer.Analyze(ptrcfg)
+		if err != nil { // not a user-input problem if it fails, see Analyze doc.
+			panic(fmt.Errorf("pointer analysis failed %v", err))
+		}
+		return result.CallGraph
+	case StaticAnalysis:
+		return static.CallGraph(data.Prog)
+	case ClassHierarchyAnalysis:
+		return cha.CallGraph(data.Prog)
+	case RapidTypeAnalysis:
+		var roots []*ssa.Function
+		for _, m := range data.Mains {
+			roots = append(roots, m.Func("init"), m.Func("main"))
+		}
+		return rta.Analyze(roots, true).CallGraph
+	default:
+		return nil
+	}
+}
